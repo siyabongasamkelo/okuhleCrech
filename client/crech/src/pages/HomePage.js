387cd61a90cc411421c9a6ae6ad7b2833d32d392
siyabonga <siyabongasamkelociam@gmail.com>
2024-03-06 13:35:15 -0800

    feat: added about us page

--- client/crech/src/pages/HomePage.js
+++ client/crech/src/pages/HomePage.js
@@ -1,9 +1,11 @@
+import AboutUs from "../components/home/AboutUs";
 import Home from "../components/home/Home";
 
 const HomePage = () => {
   return (
     <>
       <Home />
+      <AboutUs />
     </>
   );
 };
