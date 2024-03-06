d91c8e25df9985f5766fe46f1378e5453f3fec5e
siyabonga <siyabongasamkelociam@gmail.com>
2024-03-06 12:39:44 -0800

    refactor: made my code more shorter by avoiding repeating code

--- client/crech/src/components/home/Home.styled.js
+++ client/crech/src/components/home/Home.styled.js
@@ -2,31 +2,74 @@ import styled from "styled-components";
 
 export const HomeCover = styled.section`
   width: 100%;
+  font-family: "Anta", sans-serif;
+  font-style: normal;
+`;
+
+export const HeroSection = styled.div`
+  display: flex;
+  flex-direction: column-reverse;
+  @media only screen and (min-width: 992px) {
+    flex-direction: row;
+  }
 `;
 
 export const TextContainer = styled.div`
-  width: 50%;
-  margin-top: 50%;
+  width: 100%;
+  margin-top: -8%;
+  text-align: center;
+  @media only screen and (min-width: 992px) {
+    text-align: left;
+    width: 55%;
+    margin-top: 10%;
+  }
 `;
 
 export const H1 = styled.h1`
-  font-size: 64px;
+  font-size: 28px;
   color: white;
+  @media only screen and (min-width: 992px) {
+    font-size: 64px;
+  }
 `;
 
 export const H3 = styled.h3`
-  font-size: 42px;
   color: white;
+  font-size: 18px;
+
+  @media only screen and (min-width: 992px) {
+    font-size: 42px;
+  }
 `;
 
 export const P = styled.p`
   line-height: 1.5;
-  padding-top: 5%;
+  color: rgba(225, 225, 225, 0.7);
+  font-size: 12px;
+  @media only screen and (min-width: 992px) {
+    padding-top: 5%;
+    font-size: 16px;
+  }
+`;
+
+export const ParagraphCover = styled.div`
+  width: 80%;
+  margin-left: 10%;
+  @media only screen and (min-width: 992px) {
+    width: 55%;
+    margin-left: 0;
+  }
 `;
 
 export const ImageContainer = styled.div`
-  width: 50%;
-  margin-top: 80%;
+  width: 90%;
+  margin-left: 5%;
+  margin-top: 15%;
+  @media only screen and (min-width: 992px) {
+    width: 40%;
+    margin-top: 10%;
+    margin-left: 10%;
+  }
 `;
 
 export const FlexBox = styled.div`
@@ -34,8 +77,13 @@ export const FlexBox = styled.div`
   justify-content: space-between;
   img {
     width: 60%;
-    height: 30vh;
+    height: 20vh;
     object-fit: cover;
+  }
+  @media only screen and (min-width: 992px) {
+    img {
+      height: 30vh;
+    }
   }
 `;
 export const FlexBox1 = styled.div`
@@ -43,8 +91,13 @@ export const FlexBox1 = styled.div`
   justify-content: space-between;
   img {
     width: 40%;
-    height: 20vh;
+    height: 15vh;
     object-fit: cover;
+  }
+  @media only screen and (min-width: 992px) {
+    img {
+      height: 20vh;
+    }
   }
 `;
 
@@ -52,12 +105,20 @@ export const FlexBox3 = styled.div`
   display: flex;
   .div1 {
     width: 40%;
-    height: 10vh;
+    height: 7vh;
   }
   .div2 {
+    height: 7vh;
     width: 20%;
-    height: 10vh;
     background-color: #0c2d57;
+  }
+  @media only screen and (min-width: 992px) {
+    .div1 {
+      height: 10vh;
+    }
+    .div2 {
+      height: 10vh;
+    }
   }
 `;
 
@@ -65,12 +126,20 @@ export const FlexBox4 = styled.div`
   display: flex;
   img {
     width: 40%;
-    height: 20vh;
+    height: 10vh;
     object-fit: cover;
   }
   .div2 {
     width: 40%;
     height: 20vh;
+  }
+  @media only screen and (min-width: 992px) {
+    img {
+      height: 20vh;
+    }
+    .div2 {
+      height: 20vh;
+    }
   }
 `;
 
@@ -91,4 +160,8 @@ export const MyButton = styled.button`
   font-size: 16px;
   margin: 15px 2px;
   cursor: pointer;
+  transition: 0.5s ease-in-out;
+  &:hover {
+    background-color: rgba(227, 101, 29, 0.5);
+  }
 `;
