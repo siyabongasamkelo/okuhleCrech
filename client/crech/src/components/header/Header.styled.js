e5e4751e84d164cd0e4550bb34d06c0ba96d3acf
siyabonga <siyabongasamkelociam@gmail.com>
2024-03-06 12:35:31 -0800

    feat: made header responsive for all devices

--- client/crech/src/components/header/Header.styled.js
+++ client/crech/src/components/header/Header.styled.js
@@ -13,31 +13,52 @@ export const HeaderCover = styled.header`
 
 export const Logo = styled.div`
   h3 {
+    display: none;
     font-size: 14px;
     color: ${({ theme }) => theme.light.smallText};
+  }
+  svg {
+    transform: scale(210%);
+  }
+  .mobileSvg,
+  .mobileLogo {
+    display: block;
+  }
+
+  @media only screen and (min-width: 992px) {
+    h3 {
+      display: block;
+    }
+    .mobileSvg,
+    .mobileLogo {
+      display: none;
+    }
   }
 `;
 
 export const Links = styled.div`
-  display: flex;
-  justify-content: space-between;
   a {
-    padding-left: 50px;
-    text-decoration: none;
-    color: ${({ theme }) => theme.light.smallText};
+    display: none;
+  }
+
+  @media only screen and (min-width: 992px) {
+    display: flex;
+    justify-content: space-between;
+    a {
+      display: block;
+      padding-left: 50px;
+      text-decoration: none;
+      color: ${({ theme }) => theme.light.smallText};
+    }
   }
 `;
 
-export const SocialMediaIcons = styled.div`
-  height: 100px;
-  width: 100px;
-`;
+export const SocialMediaIcons = styled.div``;
 
 export const Icons = styled.div`
   width: 40px;
   height: 200px;
-  margin-top: 50%;
-  border-radius: 10px;
+  margin-top: 450%;
   display: flex;
   flex-direction: column;
   align-items: center;
