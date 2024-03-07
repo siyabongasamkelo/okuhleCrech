e1ba12ad2a2dcf09ca8085836831066e6fe2a111
siyabonga <siyabongasamkelociam@gmail.com>
2024-03-06 17:00:31 -0800

    feat: added ourServices & Contacts

--- client/crech/src/pages/HomePage.js
+++ client/crech/src/pages/HomePage.js
@@ -1,11 +1,15 @@
 import AboutUs from "../components/home/AboutUs";
+import Contact from "../components/home/Contact";
 import Home from "../components/home/Home";
+import OurServices from "../components/home/OurServices";
 
 const HomePage = () => {
   return (
     <>
       <Home />
       <AboutUs />
+      <OurServices />
+      <Contact />
     </>
   );
 };
