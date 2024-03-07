75d15a41525ce6087f2d3ca126b6eaf32ab28d3e
siyabonga <siyabongasamkelociam@gmail.com>
2024-03-07 11:04:39 -0800

    feat: made contact details responsive for mobile devices

--- client/crech/src/components/home/Contact.styled.js
+++ client/crech/src/components/home/Contact.styled.js
@@ -2,10 +2,16 @@ import styled from "styled-components";
 
 export const ContactCover = styled.section`
   margin-top: 10%;
+  @media only screen and (max-width: 600px) {
+    text-align: center;
+  }
 `;
 
 export const ContactContainer = styled.div`
   display: flex;
+  @media only screen and (max-width: 600px) {
+    flex-direction: column;
+  }
 `;
 
 export const Flexbox = styled.div`
@@ -21,18 +27,31 @@ export const Flexbox = styled.div`
 
 export const ContactDetails = styled.div`
   width: 35%;
+  @media only screen and (max-width: 600px) {
+    width: 90%;
+    margin-left: 5%;
+  }
 `;
 
 export const ContactForm = styled.form`
   width: 60%;
   display: flex;
   flex-direction: column;
+  @media only screen and (max-width: 600px) {
+    width: 90%;
+    margin-top: 10%;
+    margin-left: 5%;
+  }
 `;
 export const Input = styled.input`
   width: 60%;
   height: 40px;
   margin-left: 40%;
   border: 2px solid ${({ theme }) => theme.light.secondary};
+  @media only screen and (max-width: 600px) {
+    width: 100%;
+    margin-left: 0;
+  }
 `;
 
 export const TextArea = styled.textarea`
@@ -41,6 +60,10 @@ export const TextArea = styled.textarea`
   margin-top: 3%;
   margin-left: 40%;
   border: 2px solid ${({ theme }) => theme.light.secondary};
+  @media only screen and (max-width: 600px) {
+    width: 100%;
+    margin-left: 0;
+  }
 `;
 
 export const SubmitButton = styled.button`
@@ -56,5 +79,9 @@ export const SubmitButton = styled.button`
   transition: 0.5s ease-in-out;
   &:hover {
     background-color: rgba(227, 101, 29, 0.5);
+  }
+  @media only screen and (max-width: 600px) {
+    width: 100%;
+    margin-left: 0;
   }
 `;
