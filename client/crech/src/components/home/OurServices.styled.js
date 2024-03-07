a22e092cfbcb0e09d1b93602d80f56afaa9036ae
siyabonga <siyabongasamkelociam@gmail.com>
2024-03-06 16:57:27 -0800

    feat: ourservices styles

---
+++ client/crech/src/components/home/OurServices.styled.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+import { AboutContainer } from "./About.styled";
+
+export const OurServicesCover = styled.section`
+  font-family: "Anta", sans-serif;
+  font-style: normal;
+  margin-top: 7%;
+  @media only screen and (max-width: 600px) {
+    text-align: center;
+    margin-top: 15%;
+  }
+`;
+
+export const OurServicesContainer = styled(AboutContainer)``;
